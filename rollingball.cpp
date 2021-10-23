185d691dd55fb8f576ac533488ae7242feb71e3d
CrappyCracker <60387086+CrappyCracker@users.noreply.github.com>
2021-10-23 22:10:56 +0200

    Setting up things

--- rollingball.cpp
+++ rollingball.cpp
@@ -12,9 +12,43 @@ RollingBall::~RollingBall()
 }
 void RollingBall::move(float dt)
 {
-    std::vector<gsml::Vertex>& vertices = dynamic_cast<TriangleSurface*>(triangle_surface)->get_vertices();
+    //std::vector<gsml::Vertex>& vertices = dynamic_cast<TriangleSurface*>(triangle_surface)->get_vertices();
 
     mMatrix = mPosition * mScale;
+
+}
+
+bool RollingBall::findTriangle(unsigned int index, gsml::Vector3d &position, gsml::Vector3d &outBaryCords, gsml::Vector3d &outP, gsml::Vector3d &outQ, gsml::Vector3d &outR) const
+{
+    gsml::Vector3d p, q, r;
+    std::vector<gsml::Vertex>& vertices = dynamic_cast<TriangleSurface*>(triangle_surface)->get_vertices();
+
+    p = gsml::Vector3d(vertices[index].getXYZ().x, vertices[index].getXYZ().y, 1.f);
+    q = gsml::Vector3d(vertices[index+1].getXYZ().x, vertices[index+1].getXYZ().y, 1.f);
+    r = gsml::Vector3d(vertices[index+2].getXYZ().x, vertices[index+2].getXYZ().y, 1.f);
+
+    outBaryCords = position.barycentricCoordinates(p, q, r);
+
+    if (outBaryCords.x >= 0 && outBaryCords.y >= 0 && outBaryCords.z >= 0)
+    {
+        outP = vertices[index].getXYZ();
+        outQ = vertices[index+1].getXYZ();
+        outR = vertices[index+2].getXYZ();
+        return true;
+    }
+
+    return false;
+}
+
+float RollingBall::getHeight() const
+{
+    gsml::Vector3d BaryCords{};
+    std::vector<gsml::Vertex>& vertices = dynamic_cast<TriangleSurface*>(triangle_surface)->get_vertices();
+
+    for (unsigned int i{}; i < vertices.size(); i += 3)
+    {
+        if (findTriangle(i, ))
+    }
 
 }
 
@@ -49,3 +83,5 @@ void RollingBall::draw()
    glUniformMatrix4fv( mMatrixUniform, 1, GL_TRUE, mMatrix.constData());
    glDrawArrays(GL_TRIANGLES, 0, mVertices.size());//mVertices.size());
 }
+
+
--- rollingball.h
+++ rollingball.h
@@ -9,10 +9,13 @@ class RollingBall : public OctahedronBall
 public:
     RollingBall(int n);
     ~RollingBall() override;
+
     void init(GLint matrixUniform) override;
     void draw() override;
     void move(float dt) override;
     void setSurface(VisualObject* surface) { triangle_surface = surface; }
+    bool findTriangle(unsigned int index, gsml::Vector3d& position, gsml::Vector3d& outBaryCords, gsml::Vector3d& outP, gsml::Vector3d& outQ, gsml::Vector3d& outR) const;
+    float getHeight() const;
 protected:
     VisualObject* triangle_surface;
 private:
